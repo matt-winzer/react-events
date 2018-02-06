15fa1bbce946954e2576b0154588fa2aef7d4f23
matt-winzer <matt.winzer@gmail.com>
2018-02-05 20:31:53 -0700

    created List component, loading data in app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import List from './components/List'
 
 class App extends Component {
-  render() {
+  state = { data: [], baseUrl: 'http://localhost:3000/mens'}
+
+  componentDidMount() {
+    fetch(this.state.baseUrl)
+      .then(response => response.json())
+      .then(data => this.setState({ data: data }))
+  }
+
+  render() {    
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">FEF Concepts</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <List sunglasses={this.state.data} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const List = (props) => {
+  console.log(props);
+  
+  return (
+    <h1>Hi from the List!</h1>
+  )
+}
+
+export default List
