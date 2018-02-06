a564dae1f2c130eeb57faa4b10ab9b7679a23780
matt-winzer <matt.winzer@gmail.com>
2018-02-06 09:40:32 -0700

    loading screen before data is loaded

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import logo from './logo.svg'
 import './App.css'
 import List from './components/List'
 
-const devUrl = 'http://localhost:3000/mens'
+const devUrl = 'http://localhost:3000/goggles'
 
 class App extends Component {
-  state = { data: [], baseUrl: devUrl}
+  state = { data: [], baseUrl: devUrl, loaded: false}
 
   componentDidMount() {
     fetch(this.state.baseUrl)
       .then(response => response.json())
-      .then(data => this.setState({ data: data }))
+      .then(data => this.setState({ data: data, loaded: true }))
   }
 
-  render() {    
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">FEF Concepts</h1>
+          <h1 className="App-title">Front-End-Framework Concepts</h1>
         </header>
-        <List sunglasses={this.state.data} />
+        {this.state.loaded ? <List sunglasses={this.state.data} /> : <h1>Loading...</h1>}
       </div>
     )
   }
